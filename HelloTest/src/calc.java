3392a8ff5e4c845cea5a647b210ad91b07e81081
Marjory <mawenying7@yahoo.co.jp>
2017-06-07 15:07:00 +0800

    upd4

--- HelloTest/src/calc.java
+++ HelloTest/src/calc.java
@@ -4,7 +4,7 @@ public class calc {
 	public static void main(String[] args) {
 		String s = "this is a new class";
 		System.out.print(s);
-		System.out.print("1");
+		System.out.print("2");
 	}
 
 }
