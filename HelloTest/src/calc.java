b39417a7ff265468c2ce97fb7cd91050da8f28cc
ma <ma-wenying@idwuhan.com>
2017-06-07 14:40:04 +0800

    upd3

--- HelloTest/src/calc.java
+++ HelloTest/src/calc.java
@@ -2,9 +2,10 @@
 public class calc {
 
 	public static void main(String[] args) {
+		System.out.print(1);
 		String s = "this is a new class";
 		System.out.print(s);
-		System.out.print("1");
+		System.out.print("2");
 	}
 
 }
