4df0c8413be27dda1760538acdb6942bd90f5dc3
Marjory <mawenying7@yahoo.co.jp>
2017-06-08 11:42:42 +0800

    test

--- HelloTest/src/calc4.java
+++ HelloTest/src/calc4.java
@@ -2,7 +2,7 @@
 public class calc4 {
 
 	public static void main(String[] args) {
-		System.out.print(1);
+		System.out.print(2);
 		String s = "this is a new class";
 		System.out.print(s);
 		System.out.print("2");
