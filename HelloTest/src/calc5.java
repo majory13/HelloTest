2a729036225c1a79e1cf1560c5c5b665e44c658a
ma <ma-wenying@idwuhan.com>
2017-06-29 15:43:32 +0800

    1

---
+++ HelloTest/src/calc5.java
@@ -0,0 +1,11 @@
+
+public class calc5 {
+
+	public static void main(String[] args) {
+		System.out.print(1);
+		String s = "this is a new class";
+		System.out.print(s);
+		System.out.print("2");
+	}
+
+}
